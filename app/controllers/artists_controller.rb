c71508526391dc57a9d4a0351381dcc32755073e
Don Mallory <dmm333@gmail.com>
2020-05-18 17:04:45 -0700

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,12 +1,19 @@
 class ArtistsController < ApplicationController
   def index
+    @artists = Artist.all
   end
 
   def show
+    @artist = Artist.find(params[:id])
   end
 
   def new
     @artist = Artist.new
+  end
+
+
+  def show_my_songs
+
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   resources :artists, :songs
+  get '/artists/:id/songs', to: "artists#show_my_songs", as: :discography
 end
